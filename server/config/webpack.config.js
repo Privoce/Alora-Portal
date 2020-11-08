d818702c904b9f1fa8ee90e5af9c0acbf1c7d02b
Liyang Zhu <1184997399@qq.com>
2020-11-09 00:44:35 +0800

    :lipstick: Merged new UI

--- server/config/webpack.config.js
+++ server/config/webpack.config.js
@@ -6,9 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WebpackBar = require("webpackbar");
 const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
 const { argv } = require("yargs");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const { PROJECT_ROOT, MINIMUM_CHROME_VERSION } = require("./env");
-
 const isDev = argv.mode !== "production";
 
 module.exports = {
@@ -123,12 +121,6 @@ module.exports = {
     }),
     new FriendlyErrorsPlugin(),
     new MiniCssExtractPlugin(),
-    new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        memoryLimit: 2048,
-        configFile: resolve(PROJECT_ROOT, "tsconfig.json"),
-      },
-    }),
     new CopyWebpackPlugin({
       patterns: [
         {
