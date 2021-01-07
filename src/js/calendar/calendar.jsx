3a388d89299ba085344e9a803578815c1a64b34f
Tulio Calil <tuliocll@gmail.com>
2021-01-07 11:08:09 -0300

    merge socket events integration and apps integration

--- src/js/calendar/calendar.jsx
+++ src/js/calendar/calendar.jsx
@@ -1,265 +1,147 @@
 import React from "react";
-import {Calendar as BigCalendar, dateFnsLocalizer, Views} from "react-big-calendar";
-import {format, getDay, parse, startOfWeek} from "date-fns";
-import {FaExternalLinkAlt, FaGoogle} from "react-icons/fa";
+import {
+  Calendar as BigCalendar,
+  dateFnsLocalizer,
+  Views,
+} from "react-big-calendar";
+import { Button, Row } from "antd";
+import {
+  FaGoogle,
+  FaExternalLinkAlt,
+  FaArrowLeft,
+  FaArrowRight,
+} from "react-icons/fa";
+import {
+  getMonth,
+  getDay,
+  startOfWeek,
+  parse,
+  format,
+  addDays,
+  subDays,
+} from "date-fns";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "../../css/calendar.less";
 
-const BACKEND_URL = "https://auth.privoce.com/";
-
 const CustomToolbar = () => {
-    return <></>;
+  return <></>;
 };
 
-const CustomTimeGutterHeader = () => {
-    return (
-        <div>
-            <h2 className="calendar--card-today">Today</h2>
-        </div>
-    );
+const CustomTimeGutterHeader = (onClick) => {
+  return (
+    <div>
+      <button className="calendar--card-today" onClick={onClick}>
+        Today
+      </button>
+    </div>
+  );
 };
 
-const CustomDateCellWrapper = () => {
-    return (
-        <div className="calendar--card-date">
-            <p>{format(new Date(), "dd MMMM, yyyy")}</p>
-        </div>
-    );
+const CustomDateCellWrapper = (date) => {
+  return (
+    <div className="calendar--card-date">
+      <p>{format(date, "dd MMMM, yyyy")}</p>
+    </div>
+  );
 };
 
 class Calendar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            historyDomains: [],
-            workspaces: [],
-            currentWorkspaceId: null,
-            user: {
-                name: "",
-                googleConnect: false,
-                token: "",
-                events: [],
-            },
-        };
-    }
-
-    getEventsFromServer = async () => {
-        const googleConnected = localStorage.getItem("googleConnect");
-        const token = localStorage.getItem("token");
-        const nickname = localStorage.getItem("nickname");
-
-        this.setState({
-            user: {
-                name: nickname,
-                googleConnect: googleConnected,
-                token,
-                events: [],
-            },
-        });
-
-        // if dont have google account connected
-        if (!googleConnected || token === "") {
-            return;
-        }
-
-        const response = await fetch(`${BACKEND_URL}user/calendar`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "x-access-token": token,
-            },
-        });
-
-        if (response.status > 204) {
-            this.setState({
-                user: {
-                    name: "",
-                    googleConnect: false,
-                    token: "",
-                    events: [],
-                },
-            });
-
-            localStorage.setItem("nickname", "");
-            localStorage.setItem("googleConnect", "false");
-            localStorage.setItem("token", "");
-
-            return alert("Session expired!");
-        }
-
-        const data = await response.json();
-
-        if (data.events && data.events.length > 0) {
-            const events = data.events.map((event) => ({
-                id: event.id,
-                start: event.start.date
-                    ? new Date(event.start.date)
-                    : new Date(event.start.dateTime),
-                end: event.end.date
-                    ? new Date(event.end.date)
-                    : new Date(event.end.dateTime),
-                title: event.summary,
-                allDay: !!event.start.date,
-            }));
-
-            console.log("calendar", {
-                user: {
-                    name: nickname,
-                    token,
-                    googleConnect: googleConnected,
-                    events,
-                },
-            });
-
-            this.setState({
-                user: {
-                    name: nickname,
-                    token,
-                    googleConnect: googleConnected,
-                    events,
-                },
-            });
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      historyDomains: [],
+      workspaces: [],
+      currentWorkspaceId: null,
+      currentDate: new Date(),
+      events: [],
     };
+  }
 
-    loginHandle = () => {
-        const globalThis = this;
+  render() {
+    const locales = {
+      "en-US": require("date-fns/locale/en-US"),
+    };
+    const localizer = dateFnsLocalizer({
+      format,
+      parse,
+      startOfWeek,
+      getDay,
+      locales,
+    });
 
-        chrome.tabs.create({
-            url: `${BACKEND_URL}auth/google?redirect=http://localhost/auth?token=`,
-        });
-
-        // we can improve this, listening only the auth tab
-        chrome.tabs.onUpdated.addListener(async function authorizationHook(
-            tabId,
-            changeInfo,
-            tab
-        ) {
-            //If you don't have the authentication tab id remove that part
-            if (tab.title.indexOf("token=") >= 0) {
-                //tab url consists of access_token
-                const url = new URL(tab.url);
-                const urlParams = new URLSearchParams(url.search);
-                const token = urlParams.get("token");
-
-                if (!token) {
-                    return;
-                }
-
-                const userResponse = await fetch(`${BACKEND_URL}auth/me`, {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "x-access-token": token,
-                    },
-                });
-
-                if (userResponse.status !== 200) {
-                    return alert("User not found");
-                }
-
-                const userData = await userResponse.json();
-
-                globalThis.setState({
-                    user: {
-                        name: userData.user.nickname,
-                        googleConnect: true,
-                        token,
-                        events: [],
-                    },
-                });
-
-                //save on localstorage
-                localStorage.setItem("nickname", userData.user.nickname);
-                localStorage.setItem("googleConnect", "true");
-                localStorage.setItem("token", token);
-
-                globalThis.getEventsFromServer();
-
-                chrome.tabs.onUpdated.removeListener(authorizationHook);
-                chrome.tabs.remove(tabId);
-            }
-        });
-    }
-
-    componentDidMount() {
-        // fetch data from api
-        this.getEventsFromServer();
-
-        const googleConnected = localStorage.getItem("googleConnect");
-        const nickname = localStorage.getItem("nickname");
-
-        this.setState({
-            user: {
-                ...this.state.user,
-                name: nickname,
-                googleConnect: googleConnected === "true",
-            },
-        });
-    }
-
-    render() {
-        const locales = {
-            "en-US": require("date-fns/locale/en-US"),
-        };
-        const localizer = dateFnsLocalizer({
-            format,
-            parse,
-            startOfWeek,
-            getDay,
-            locales,
-        });
-
-        return (
+    return (
+      <>
+        {/* We should replace them with antd's Card Component */}
+        <div className="calendar--container">
+          {this.props.user.googleConnect ? (
             <>
-                {/* We should replace them with antd's Card Component */}
-                <div className="calendar--container">
-                    {this.state.user.googleConnect ? (
-                        <>
-                            <BigCalendar
-                                className="big-calendar"
-                                style={{height: "calc(100% - 30px)"}}
-                                events={this.state.user.events}
-                                localizer={localizer}
-                                startAccessor="start"
-                                endAccessor="end"
-                                defaultView={Views.DAY}
-                                views={Views.DAY}
-                                step={30}
-                                showMultiDayTimes
-                                components={{
-                                    toolbar: CustomToolbar,
-                                    timeGutterHeader: CustomTimeGutterHeader,
-                                    dateCellWrapper: CustomDateCellWrapper,
-                                }}
-                            />
-                            <div className="link--container">
-                                <a
-                                    className="google-calendar-link"
-                                    href="https://calendar.google.com/calendar/u/0/r"
-                                    target="_blank"
-                                >
-                                    <FaExternalLinkAlt/>
-                                </a>
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            <div className="social-auth--container">
-                                <button onClick={this.loginHandle}>
-                                    <FaGoogle/>
-                                    Login with Google
-                                </button>
-                            </div>
-                        </>
-                    )}
-                </div>
+              <BigCalendar
+                className="big-calendar"
+                style={{ height: "320px" }}
+                events={this.props.user.events}
+                date={this.state.currentDate}
+                localizer={localizer}
+                startAccessor="start"
+                endAccessor="end"
+                defaultView={Views.DAY}
+                views={Views.DAY}
+                step={30}
+                showMultiDayTimes
+                components={{
+                  toolbar: CustomToolbar,
+                  timeGutterHeader: () =>
+                    CustomTimeGutterHeader(() =>
+                      this.setState({ currentDate: new Date() })
+                    ),
+                  dateCellWrapper: () =>
+                    CustomDateCellWrapper(this.state.currentDate),
+                }}
+              />
+              <div className="calendar-footer--container">
+                <Row>
+                  <Button
+                    onClick={() => {
+                      this.setState({
+                        currentDate: subDays(this.state.currentDate, 1),
+                      });
+                    }}
+                  >
+                    <FaArrowLeft />
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      this.setState({
+                        currentDate: addDays(this.state.currentDate, 1),
+                      });
+                    }}
+                  >
+                    <FaArrowRight />
+                  </Button>
+                </Row>
+                <a
+                  className="google-calendar-link"
+                  href="https://calendar.google.com/calendar/u/0/r"
+                  target="_blank"
+                >
+                  <FaExternalLinkAlt />
+                </a>
+              </div>
             </>
-        );
-    }
+          ) : (
+            <>
+              <div className="social-auth--container">
+                <button onClick={() => this.props.onLogin()}>
+                  <FaGoogle />
+                  Login with Google
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
-export {
-    Calendar,
-};
+export { Calendar };
--- src/js/launcher/launcher.jsx
+++ src/js/launcher/launcher.jsx
@@ -1,47 +1,363 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {CloseOutlined} from "@ant-design/icons";
-import {v4 as uuid} from "uuid";
+import { CloseOutlined } from "@ant-design/icons";
+import { v4 as uuid } from "uuid";
+import {
+  Avatar,
+  Button,
+  Col,
+  List,
+  Popover,
+  Row,
+  Tabs,
+  Tooltip,
+  Modal,
+  Input,
+  Card,
+} from "antd";
 
 import Style from "../../css/launcher.module.less";
-import {getFaviconUrl} from "../misc/utils";
+import { getFaviconUrl } from "../misc/utils";
 
 class Icon extends React.Component {
-    render() {
-        return (
-            <div className={Style.icon}>
-                <img src={getFaviconUrl(this.props.url)} alt="" onDragStart={event => event.preventDefault()}/>
-                <div className={Style.closeBtn}>
-                    <CloseOutlined/>
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete() {
+    this.props.deleteCallback(this.props.name);
+  }
+
+  render() {
+    return (
+      <div className={`${Style.icon} ${this.props.selected && Style.selected}`}>
+        <img
+          src={getFaviconUrl(this.props.url)}
+          alt=""
+          onDragStart={(event) => event.preventDefault()}
+        />
+        <button className={Style.closeBtn} onClick={this.handleDelete}>
+          <CloseOutlined />
+        </button>
+      </div>
+    );
+  }
 }
 
 Icon.propTypes = {
-    id: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  deleteCallback: PropTypes.func.isRequired,
+  selected: PropTypes.bool.isRequired,
 };
 
 class Launcher extends React.Component {
-    render() {
-        return (
-            <div className={Style.container}>
-                <Icon id={uuid()} url="https://www.google.com"/>
-                <Icon id={uuid()} url="https://reactjs.org/"/>
-                <Icon id={uuid()} url="https://www.youtube.com"/>
-                <Icon id={uuid()} url="https://facebook.com/"/>
-                <Icon id={uuid()} url="https://twitter.com"/>
-                <Icon id={uuid()} url="https://www.baidu.com/"/>
-                <div className={Style.icon}>
-                    <div className={Style.addBtn}>+</div>
-                </div>
-            </div>
-        );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      myApps: [
+        {
+          name: "Instagram",
+          url: "https://www.instagram.com",
+          selected: false,
+        },
+        {
+          name: "Privoce",
+          url: "https://privoce.com/",
+          selected: false,
+        },
+      ],
+      newEntryModal: false,
+      newEntryModalLoading: false,
+      newEntryModalAppUrl: "",
+      newentryModalImagePreview: "",
+      deleteAppModal: false,
+      deleteAppName: "",
+    };
+
+    this.handleNewEntryModal = this.handleNewEntryModal.bind(this);
+    this.saveMyApps = this.saveMyApps.bind(this);
+    this.loadMyApps = this.loadMyApps.bind(this);
+    this.handlePinedAppsChange = this.handlePinedAppsChange.bind(this);
+    this.handleModalUrlChange = this.handleModalUrlChange.bind(this);
+    this.handleDone = this.handleDone.bind(this);
+    this.toggleDeleteModal = this.toggleDeleteModal.bind(this);
+    this.handleDeleteApp = this.handleDeleteApp.bind(this);
+    this.deleteApp = this.deleteApp.bind(this);
+  }
+
+  handleNewEntryModal() {
+    this.setState({ newEntryModal: !this.state.newEntryModal });
+  }
+
+  saveMyApps() {
+    const saveApps = JSON.stringify(this.state.myApps);
+
+    localStorage.setItem("my-apps", saveApps);
+  }
+
+  loadMyApps() {
+    const savedApps = JSON.parse(localStorage.getItem("my-apps"));
+
+    if (!savedApps) {
+      return;
     }
+
+    this.setState({
+      myApps: savedApps,
+    });
+  }
+
+  handlePinedAppsChange(item) {
+    const findApp = this.state.myApps.find((app) => app.name === item.name);
+
+    let updatePineds = [];
+
+    if (findApp) {
+      updatePineds = this.state.myApps.map((app) => {
+        if (app.name === item.name) {
+          return {
+            ...app,
+            pined: item.pined,
+          };
+        }
+
+        return app;
+      });
+    } else {
+      updatePineds = [...this.state.myApps, item];
+    }
+
+    this.setState(
+      {
+        myApps: updatePineds,
+      },
+      () => {
+        this.saveMyApps();
+      }
+    );
+  }
+
+  handleModalUrlChange(e) {
+    this.setState({ newEntryModalAppUrl: e.target.value });
+  }
+
+  async handleDone() {
+    const { newEntryModalAppUrl, myApps } = this.state;
+    this.setState({
+      newEntryModalLoading: true,
+    });
+
+    let prefix = "";
+
+    if (
+      newEntryModalAppUrl.slice(0, 5) !== "https" ||
+      newEntryModalAppUrl.slice(0, 4) !== "http"
+    ) {
+      prefix = "https://";
+    }
+
+    try {
+      new URL(`${prefix}${newEntryModalAppUrl}`);
+
+      let tempTab;
+      chrome.tabs.create(
+        {
+          url: `${prefix}${newEntryModalAppUrl}`,
+          active: false,
+        },
+        (tab) => {
+          tempTab = tab.id;
+        }
+      );
+
+      setTimeout(() => {
+        this.setState(
+          {
+            newentryModalImagePreview: `chrome://favicon/size/128@1x/${prefix}${newEntryModalAppUrl}`,
+            newEntryModalLoading: false,
+            myApps: [
+              ...myApps,
+              {
+                name: `${prefix}${newEntryModalAppUrl}`,
+                url: `${prefix}${newEntryModalAppUrl}`,
+                icon: `chrome://favicon/size/128@1x/${prefix}${newEntryModalAppUrl}`,
+                pined: true,
+              },
+            ],
+          },
+          () => {
+            this.saveMyApps();
+          }
+        );
+
+        chrome.tabs.remove(tempTab);
+
+        //only to create a effect, see icon and close modal
+        setTimeout(() => {
+          this.handleNewEntryModal();
+        }, 500);
+      }, 2000);
+    } catch (err) {
+      this.setState({
+        newEntryModalLoading: false,
+      });
+      return;
+    }
+  }
+
+  toggleDeleteModal() {
+    if (this.state.deleteAppModal) {
+      const updateSelected = this.state.myApps.map((app) => {
+        if (app.selected) {
+          return {
+            ...app,
+            selected: false,
+          };
+        }
+        return app;
+      });
+
+      this.setState({
+        myApps: updateSelected,
+      });
+    }
+
+    this.setState({
+      deleteAppModal: !this.state.deleteAppModal,
+    });
+  }
+
+  handleDeleteApp(name) {
+    const updateSelected = this.state.myApps.map((app) => {
+      if (name === app.name) {
+        return {
+          ...app,
+          selected: true,
+        };
+      }
+      return app;
+    });
+
+    this.setState({
+      deleteAppName: name,
+      myApps: updateSelected,
+    });
+
+    this.toggleDeleteModal();
+  }
+
+  deleteApp() {
+    const newApps = this.state.myApps.filter(
+      (app) => app.name !== this.state.deleteAppName
+    );
+
+    this.setState(
+      {
+        myApps: newApps,
+      },
+      () => {
+        this.toggleDeleteModal();
+        this.saveMyApps();
+      }
+    );
+  }
+
+  componentDidMount() {
+    this.loadMyApps();
+  }
+
+  render() {
+    const {
+      newEntryModalLoading,
+      newEntryModal,
+      newentryModalImagePreview,
+      deleteAppModal,
+      deleteAppName,
+      newEntryModalAppUrl,
+    } = this.state;
+    return (
+      <div className={Style.container}>
+        {this.state.myApps.map((app) => (
+          <Icon
+            id={uuid()}
+            url={app.url}
+            name={app.name}
+            deleteCallback={this.handleDeleteApp}
+            selected={app.selected}
+          />
+        ))}
+
+        <div className={Style.icon}>
+          <button className={Style.addBtn} onClick={this.handleNewEntryModal}>
+            +
+          </button>
+        </div>
+
+        <Modal
+          title="Add a new app"
+          visible={newEntryModal}
+          onOk={this.handleNewEntryModal}
+          onCancel={this.handleNewEntryModal}
+          footer={
+            <div className="modal-footer--container">
+              <Button
+                key="remove"
+                disabled={newEntryModalLoading}
+                onClick={this.handleNewEntryModal}
+              >
+                Cancel
+              </Button>
+
+              <Button
+                type="primary"
+                loading={newEntryModalLoading}
+                key="done"
+                onClick={this.handleDone}
+              >
+                Done
+              </Button>
+            </div>
+          }
+        >
+          <div className="modal-content--container">
+            <div className="modal-contenct--image-container">
+              <img
+                src={!!newentryModalImagePreview && newentryModalImagePreview}
+              />
+              <small>Preview</small>
+            </div>
+            <div className="modal-contenct--input-container">
+              <label>URL:</label>
+              <Input
+                autoFocus
+                value={newEntryModalAppUrl}
+                onChange={this.handleModalUrlChange}
+                disabled={newEntryModalLoading}
+              />
+            </div>
+          </div>
+        </Modal>
+
+        {/* Delete app modal */}
+        <Modal
+          title="Delete app"
+          visible={deleteAppModal}
+          onOk={this.deleteApp}
+          onCancel={this.toggleDeleteModal}
+          okText="Yes"
+          cancelText="Cancel"
+          okType="danger"
+        >
+          <p>{`Delete the ${deleteAppName} ?`}</p>
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export {
-    Launcher
-};
+export { Launcher };
