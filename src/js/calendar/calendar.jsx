5a2c0f9f716fa57d73591d5c477792432841affe
Tulio Calil <tuliocll@gmail.com>
2021-01-07 15:08:16 -0300

    merge natalia refinement

--- src/js/calendar/calendar.jsx
+++ src/js/calendar/calendar.jsx
@@ -4,22 +4,14 @@ import {
   dateFnsLocalizer,
   Views,
 } from "react-big-calendar";
-import { Button, Row } from "antd";
+import { Row, Button } from "antd";
+import { format, getDay, parse, startOfWeek } from "date-fns";
 import {
-  FaGoogle,
   FaExternalLinkAlt,
-  FaArrowLeft,
+  FaGoogle,
   FaArrowRight,
+  FaArrowLeft,
 } from "react-icons/fa";
-import {
-  getMonth,
-  getDay,
-  startOfWeek,
-  parse,
-  format,
-  addDays,
-  subDays,
-} from "date-fns";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "../../css/calendar.less";
@@ -71,75 +63,77 @@ class Calendar extends React.Component {
     });
 
     return (
-      <>
-        {/* We should replace them with antd's Card Component */}
-        <div className="calendar--container">
-          {this.props.user.googleConnect ? (
-            <>
-              <BigCalendar
-                className="big-calendar"
-                style={{ height: "320px" }}
-                events={this.props.user.events}
-                date={this.state.currentDate}
-                localizer={localizer}
-                startAccessor="start"
-                endAccessor="end"
-                defaultView={Views.DAY}
-                views={Views.DAY}
-                step={30}
-                showMultiDayTimes
-                components={{
-                  toolbar: CustomToolbar,
-                  timeGutterHeader: () =>
-                    CustomTimeGutterHeader(() =>
-                      this.setState({ currentDate: new Date() })
-                    ),
-                  dateCellWrapper: () =>
-                    CustomDateCellWrapper(this.state.currentDate),
-                }}
-              />
-              <div className="calendar-footer--container">
-                <Row>
-                  <Button
-                    onClick={() => {
-                      this.setState({
-                        currentDate: subDays(this.state.currentDate, 1),
-                      });
-                    }}
-                  >
-                    <FaArrowLeft />
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      this.setState({
-                        currentDate: addDays(this.state.currentDate, 1),
-                      });
-                    }}
-                  >
-                    <FaArrowRight />
-                  </Button>
-                </Row>
-                <a
-                  className="google-calendar-link"
-                  href="https://calendar.google.com/calendar/u/0/r"
-                  target="_blank"
-                >
-                  <FaExternalLinkAlt />
-                </a>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="social-auth--container">
-                <button onClick={() => this.props.onLogin()}>
-                  <FaGoogle />
-                  Login with Google
-                </button>
-              </div>
-            </>
-          )}
+      <div className="calendar--container">
+        <div className="calendar-footer--container">
+          <Row className="header-left">
+            <Button
+              className="arrow--btn"
+              onClick={() => {
+                this.setState({
+                  currentDate: subDays(this.state.currentDate, 1),
+                });
+              }}
+            >
+              <FaArrowLeft />
+            </Button>
+            <Button
+              className="arrow--btn"
+              onClick={() => {
+                this.setState({
+                  currentDate: addDays(this.state.currentDate, 1),
+                });
+              }}
+            >
+              <FaArrowRight />
+            </Button>
+            <div className="header-date">
+              <p>{format(this.state.currentDate, "dd MMMM, yyyy")}</p>
+            </div>
+          </Row>
+          <div>
+            <button
+              className="calendar--card-today"
+              style={this.state.buttonStyle}
+              onClick={() => {
+                this.setState({
+                  currentDate: new Date(),
+                });
+              }}
+            >
+              Today
+            </button>
+          </div>
         </div>
-      </>
+
+        {this.props.user.googleConnect ? (
+          <div className="events--container">
+            {this.props.user.events.map(
+              (item) =>
+                isTheSameDay(item.start, this.state.currentDate) && (
+                  <div className="event--card">
+                    <p>{item.title}</p>
+                  </div>
+                )
+            )}
+          </div>
+        ) : (
+          <>
+            <div className="social-auth--container">
+              <button onClick={this.loginHandle}>
+                <FaGoogle />
+                Login with Google
+              </button>
+            </div>
+          </>
+        )}
+        <a
+          className="google-calendar-link"
+          href="https://calendar.google.com/calendar/u/0/r"
+          target="_blank"
+        >
+          <FaExternalLinkAlt />
+        </a>
+      </div>
     );
   }
 }
