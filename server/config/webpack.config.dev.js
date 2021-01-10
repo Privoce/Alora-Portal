b520acd1e735ae06804bab31219bf6633abe2a97
Liyang Zhu <1184997399@qq.com>
2021-01-10 16:10:25 +0800

    :wrench: resolved react hook errors

--- server/config/webpack.config.dev.js
+++ server/config/webpack.config.dev.js
@@ -1,31 +1,32 @@
-const {HotModuleReplacementPlugin} = require('webpack');
-const {merge} = require('webpack-merge');
-const {resolve} = require('path');
-const baseConfig = require('./webpack.config');
-const {HOST, PORT, HMR_PATH, CONTENT_SCRIPT_CHUNKS, BACKGROUND_CHUNK} = require('./env');
+const {HotModuleReplacementPlugin} = require("webpack");
+const {merge} = require("webpack-merge");
+const {resolve} = require("path");
+const baseConfig = require("./webpack.config");
+const {HOST, PORT, HMR_PATH, CONTENT_SCRIPT_CHUNKS, BACKGROUND_CHUNK} = require("./env");
 
 const hmrUrl = encodeURIComponent(`http://${HOST}:${PORT}${HMR_PATH}`);
 const hmrClient = `webpack-hot-middleware/client?path=${hmrUrl}&reload=true&overlay=true`;
-const reactHotLoader = 'react-hot-loader/patch';
-const arrContentScriptClient = resolve(__dirname, '../util/arrContentScriptClient.js');
-const arrBackgroundClient = resolve(__dirname, '../util/arrBackgroundClient.js');
+const reactHotLoader = "react-hot-loader/patch";
+const arrContentScriptClient = resolve(__dirname, "../util/arrContentScriptClient.js");
+const arrBackgroundClient = resolve(__dirname, "../util/arrBackgroundClient.js");
 
-let patchedEntry = {};
 for (let entryName in baseConfig.entry) {
     if (CONTENT_SCRIPT_CHUNKS.includes(entryName)) {
-        patchedEntry[entryName] = [arrContentScriptClient];
+        baseConfig.entry[entryName].unshift(arrContentScriptClient);
     } else if (BACKGROUND_CHUNK === entryName) {
-        patchedEntry[entryName] = [hmrClient, arrBackgroundClient];
+        baseConfig.entry[entryName].unshift(hmrClient);
+        baseConfig.entry[entryName].unshift(arrBackgroundClient);
     } else {
-        patchedEntry[entryName] = [hmrClient, reactHotLoader];
+        baseConfig.entry[entryName].unshift(reactHotLoader);
+        baseConfig.entry[entryName].unshift(hmrClient);
     }
 }
 
 module.exports = merge(baseConfig, {
-    mode: 'development',
-    entry: patchedEntry,
-    devtool: 'eval-cheap-module-source-map',
+    mode: "development",
+    devtool: "eval-cheap-module-source-map",
+    stats: "none",
     plugins: [
-        new HotModuleReplacementPlugin()
-    ]
+        new HotModuleReplacementPlugin(),
+    ],
 });
