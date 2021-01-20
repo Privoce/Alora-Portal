bfd95d5640ad6906a2f5a1790f57d3e045dc86e4
suhan1996 <hs2657@nyu.edu>
2021-01-20 15:35:14 -0500

    i18n:zh_CN

--- public/locales/i18n.js
+++ public/locales/i18n.js
@@ -4,6 +4,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { TRANSLATIONS_PT } from './pt/translations.js';
 import { TRANSLATIONS_ES } from './es/translations.js';
 import { TRANSLATIONS_EN } from './en/translations.js';
+import { TRANSLATIONS_zh_CN } from './zh_CN/translations.js';
 
 i18n
   .use(LanguageDetector)
@@ -19,6 +20,9 @@ i18n
       es: {
         translation: TRANSLATIONS_ES,
       },
+      zh_CN:{
+        translation: TRANSLATIONS_zh_CN,
+      }
     },
     fallbackLng: 'en',
   });
